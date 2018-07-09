40d4dad3683091ddd81bebdb4d9dbd4ab176ccf2
mohammad awad <ahmad.y.saleh@gmail.com>
2018-07-09 14:23:00 +0300

    test commit

---
+++ src/main/java/com/progressoft/induction/tp/Validation.java
@@ -0,0 +1,34 @@
+package com.progressoft.induction.tp;
+
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+class Validation {
+    private TransactionForm form;
+
+    Validation(TransactionForm form) {
+        this.form = form;
+    }
+
+    List<Violation> isValid() {
+        String amt = form.getAmount().trim();
+        String type = form.getType().trim();
+        String narration = form.getNarration();
+        List<Violation> errorsArray = new ArrayList<>();
+
+        try {
+            if (new BigDecimal(amt).compareTo(BigDecimal.ZERO) <= 0)
+                errorsArray.add(new Violation(form.getIndex(), "amount"));
+        } catch (NumberFormatException e) {
+            errorsArray.add(new Violation(form.getIndex(), "amount"));
+        }
+
+        if (!type.equals("D") && !type.equals("C"))
+            errorsArray.add(new Violation(form.getIndex(), "type"));
+        if (narration == null || narration.trim().length() == 0)
+            errorsArray.add(new Violation(form.getIndex(), "narration"));
+        return errorsArray;
+    }
+}
